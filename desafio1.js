bf56e57a0febd69c47980b6ac67b969124b65aa9
Eduardo <eduardoc.santayana@gmail.com>
2024-01-08 16:16:42 -0300

    Iniciando o desafio

---
+++ desafio1.js
@@ -0,0 +1,12 @@
+/*
+## Objetivos 
+Crie uma variável para armazenar o nome e a quantidade de XP de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+1. Se XP for menor do que 1.000 = Ferro
+1. Se XP for entre  1.001 e 2.200 = Bronze
+1. Se XP for entre 2.001 e 5.000= Prata Ouro
+1. Se XP for entre 5001 e 8.000 = Platina Diamante
+1. Se XP for entre 8001 e 9.000 = Ascendente
+1. Se XP for entre 9001 e 10.000 = Imortal
+1. Se XP for maior ou igual e 10.001 = Radiante 
+*/
